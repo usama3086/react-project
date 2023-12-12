20999373f509b8c22b09f5849c17daa680f7aeb2
usama3086 <usamakhalid212212@gmail.com>
2023-12-12 16:34:01 +0500

    textutiles

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import React, { useState } from 'react';
+import Alert from './components/Alert';
+import {
+  BrowserRouter as Router,
+  Switch,
+  
+} from "react-router-dom";
+
+ 
+function App() {
+  const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type)=>{
+      setAlert({
+        msg: message,
+        type: type
+      })
+      setTimeout(() => {
+          setAlert(null);
+      }, 1500);
+  }
+
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+      showAlert("Dark mode has been enabled", "success");
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled", "success");
+    }
+  }
+  return (
+    <>
+    <Router>
+    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} key={new Date()} />
+    <Alert alert={alert}/>
+    <div className="container my-3">
+    <Switch>
+  
+       
+            <TextForm showAlert={showAlert} heading=" TextUtils" mode={mode}/>
+        
+    </Switch>
+    </div>
+    </Router>
+    </> 
+  );
+}
+
+export default App;
